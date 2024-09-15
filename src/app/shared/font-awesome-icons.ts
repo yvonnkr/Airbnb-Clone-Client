0ced51498dc5103e876c6cde013229ce19d8ec99
yvonnkr <yvonnkr86@gmail.com>
2024-09-15 19:22:51 +0100

    add packages primeng, primeflex, fontawesome and add base scss

---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,6 @@
+import {IconDefinition} from "@fortawesome/fontawesome-svg-core";
+import {faAirbnb} from "@fortawesome/free-brands-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faAirbnb
+]
